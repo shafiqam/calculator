9fd46c96231b7caf76c03afbe5598fa03cfe1cbe
Shafiq Mohammed <shafiqsnss@gmail.com>
2024-10-18 21:08:40 -0400

    separate number and symbol buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
+function handleSymbol(value) {
+  console.log('symbol');
+}
+
+function handleNumber(value) {
+  console.log('number');
+}
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
 function init() {
   document
     .querySelector('.buttons')
